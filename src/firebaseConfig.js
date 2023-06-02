92053d226921e9c64b61b3d015aa3d6eb7850e33
Priyanshu Burman <75599040+priyanshuburman@users.noreply.github.com>
2023-06-03 01:52:24 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeEAmoY-YGpCHuhbxZ4v-w88d2CJubz3g",
-  authDomain: "docs-acd63.firebaseapp.com",
-  projectId: "docs-acd63",
-  storageBucket: "docs-acd63.appspot.com",
-  messagingSenderId: "561863652393",
-  appId: "1:561863652393:web:07e07a7cd19503bf99d838"
+  apiKey: "AIzaSyDx8QVwyhlNIKH82MG3IXW9_aKsEyPI_vc",
+  authDomain: "docsclone-f7046.firebaseapp.com",
+  projectId: "docsclone-f7046",
+  storageBucket: "docsclone-f7046.appspot.com",
+  messagingSenderId: "212498342811",
+  appId: "1:212498342811:web:49417c1d2516543eccf73e",
+  measurementId: "G-DEMQCE8T8B"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
